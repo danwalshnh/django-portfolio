801572462be19102c1eb5127f19972252cf3e8d4
dan walsh <dwalsh@concordanalyticsllc.com>
2020-08-23 17:17:00 -0400

    Fix url errors on portfolio

--- journal/urls.py
+++ journal/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
-
+from journal import views
+app_name = "journal"
 urlpatterns = [
     path("",views.journal_index, name="journal_index"),
     path("<int:pk>/", views.journal_detail, name="journal_detail"),
-    path("<category>/",views.journal_category, name="journal_category"),
+    path("<category>/", views.journal_category, name="journal_category"),
 ]
--- journal/views.py
+++ journal/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from journal.models import Post
 # Create your views here.
+
+
 def journal_index(request):
     posts = Post.objects.all().order_by('-created_on')
     context = {
-        "posts": posts,
+        "posts": posts
     }
-    return render(request, "journal_index.html",context)
+    return render(request, "journal/journal_index.html", context)
+
 
 def journal_category(request, category):
     posts = Post.objects.filter(
@@ -18,11 +21,12 @@ def journal_category(request, category):
         "category": category,
         "posts": posts
     }
-    return render(request, "journal_category.html", context)
+    return render(request, "journal/journal_category.html", context)
+
 
 def journal_detail(request, pk):
     post = Post.objects.get(pk=pk)
     context = {
-        "post":post,
+        "post": post,
     }
-    return render(request, "journal_detail.html", context)
+    return render(request, "journal/journal_detail.html", context)
