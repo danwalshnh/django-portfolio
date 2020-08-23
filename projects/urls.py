c25551b4a73deba9666316d7a8b03574023e0675
dan walsh <dwalsh@concordanalyticsllc.com>
2020-08-23 11:38:31 -0400

    Add templates and projects.

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,8 @@ from typing import List, Any
 from django.urls import path
 from projects import views
 
+app_name = "projects"
 urlpatterns = [
-    path('', views.project_list),
+    path('', views.all_projects,name='all_projects'),
+    path('<int:pk>',views.project_detail, name='project_detail'),
     ]
